6ff7841b3f67b7d5ed0f8c639200880411a38e3c
akhil <akhilr37@gmail.com>
2022-04-30 05:53:26 +0000

    all functionalities added successfully

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import RegistrationForm from './components/RegistrationForm'
 import './App.css'
-import OnsubmitSuccess from './components/SubmitSuccess'
 
 const App = () => <RegistrationForm />
 
--- src/components/RegistrationForm/index.js
+++ src/components/RegistrationForm/index.js
@@ -9,6 +9,7 @@ class RegistrationForm extends Component {
     lastNameRequired: false,
     firstNameBorder: 'inputEl',
     lastnameBorder: 'inputEl',
+    isSubmitted: false,
   }
 
   onFirstnameTyping = event => {
@@ -39,9 +40,7 @@ class RegistrationForm extends Component {
     }
   }
 
-  totalPageRender = () => {}
-
-  render() {
+  totalPageRender = () => {
     const {
       firstName,
       lastName,
@@ -51,40 +50,110 @@ class RegistrationForm extends Component {
       lastnameBorder,
     } = this.state
     return (
+      <>
+        <label className="labelText" htmlFor="firstNameInput">
+          FIRST NAME
+        </label>
+        <input
+          className={`${firstNameBorder}`}
+          type="text"
+          placeholder="First Name"
+          onChange={this.onFirstnameTyping}
+          value={firstName}
+          onBlur={this.onblurFirstName}
+          id="firstNameInput"
+        />
+        {firstNameRequired && <p className="reqText">Required</p>}
+
+        <label className="labelTextL" htmlFor="lastNameInput">
+          LAST NAME
+        </label>
+        <input
+          className={`${lastnameBorder}`}
+          type="text"
+          placeholder="Last Name"
+          onChange={this.onLastnameTyping}
+          value={lastName}
+          onBlur={this.onblurLastName}
+          id="lastNameInput"
+        />
+        {lastNameRequired && <p className="reqText">Required</p>}
+      </>
+    )
+  }
+
+  successRender = () => (
+    <div className="successContainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+        alt="success"
+        className="imgSize"
+      />
+      <p>Submitted Successfully</p>
+    </div>
+  )
+
+  validateLastName = () => {
+    const {lastName} = this.state
+
+    return lastName !== ''
+  }
+
+  validateFirstName = () => {
+    const {firstName} = this.state
+
+    return firstName !== ''
+  }
+
+  onSubmitClick = event => {
+    event.preventDefault()
+    const isValidFirstName = this.validateFirstName()
+    const isValidLastName = this.validateLastName()
+
+    if (isValidFirstName && isValidLastName) {
+      event.preventDefault()
+      this.setState(prevVal => ({
+        isSubmitted: !prevVal.isSubmitted,
+        firstName: '',
+        lastName: '',
+        firstNameBorder: 'inputEl',
+        lastnameBorder: 'inputEl',
+      }))
+    } else {
+      this.setState({
+        firstName: '',
+        lastName: '',
+        firstNameRequired: !this.validateFirstName(),
+        lastNameRequired: !this.validateLastName(),
+        firstNameBorder: 'inputEl2',
+        lastnameBorder: 'inputEl2',
+        isSubmitted: false,
+      })
+    }
+  }
+
+  render() {
+    let buttonText
+    const {isSubmitted} = this.state
+    if (isSubmitted === true) {
+      buttonText = 'Submit Another Response'
+    } else {
+      buttonText = 'Submit'
+    }
+
+    return (
       <div className="totalContainer">
         <div className="contentContainer">
           <h1 className="heading">Registration</h1>
           <div className="inputsContainer">
             <form className="formContainer">
-              <label className="labelText" htmlFor="firstNameInput">
-                FIRST NAME
-              </label>
-              <input
-                className={`${firstNameBorder}`}
-                type="text"
-                placeholder="First Name"
-                onChange={this.onFirstnameTyping}
-                value={firstName}
-                onBlur={this.onblurFirstName}
-                id="firstNameInput"
-              />
-              {firstNameRequired && <p className="reqText">Required</p>}
-
-              <label className="labelTextL" htmlFor="lastNameInput">
-                LAST NAME
-              </label>
-              <input
-                className={`${lastnameBorder}`}
-                type="text"
-                placeholder="Last Name"
-                onChange={this.onLastnameTyping}
-                value={lastName}
-                onBlur={this.onblurLastName}
-                id="lastNameInput"
-              />
-              {lastNameRequired && <p className="reqText">Required</p>}
-              <button className="submitButton" type="submit">
-                Submit
+              {isSubmitted ? this.successRender() : this.totalPageRender()}
+              <button
+                className="submitButton"
+                type="submit"
+                onClick={this.onSubmitClick}
+              >
+                {buttonText}
               </button>
             </form>
           </div>
